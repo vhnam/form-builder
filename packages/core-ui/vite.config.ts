6d5b04e98dc788b5a4ff16061825618aeff24b82
Nam Vo <vhnam2504@gmail.com>
2025-09-06 16:16:44 +0700

    feat: setup Tailwind v4

--- packages/core-ui/vite.config.ts
+++ packages/core-ui/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
 import path from "path";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
@@ -9,6 +10,7 @@ export default defineConfig({
     dts({
       insertTypesEntry: true,
     }),
+    tailwindcss(),
   ],
   build: {
     lib: {
